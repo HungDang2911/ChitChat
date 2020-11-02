a918914f1c46d1659cf3da7d9879ee33d44476c5
Đặng Tuấn Hưng <hung291100@gmail.com>
2020-11-02 16:48:30 +0700

    fixed api

--- app/services/api/api.types.ts
+++ app/services/api/api.types.ts
@@ -1,9 +1,11 @@
 import { GeneralApiProblem } from "./api-problem"
 
-export interface User {
-  id: number
-  name: string
+export interface Token {
+  accessToken: string,
+  refreshToken: string
 }
 
-export type GetUsersResult = { kind: "ok"; users: User[] } | GeneralApiProblem
-export type GetUserResult = { kind: "ok"; user: User } | GeneralApiProblem
+export type loginResult = { kind: "ok"; token: Token } | GeneralApiProblem
+
+// export type GetUsersResult = { kind: "ok"; users: User[] } | GeneralApiProblem
+// export type GetUserResult = { kind: "ok"; user: User } | GeneralApiProblem
